e0a9d622efa8bd8b691aa392d25c4314e7559b73
renniemaharaj <rvesprey@gmail.com>
2025-10-15 09:58:46 -0400

    moved commands

---
+++ internal/commandCli/cmds.go
@@ -0,0 +1,24 @@
+package commandCli
+
+import (
+	"github.com/codegangsta/cli"
+	"github.com/ystyle/jvms/internal/entity"
+)
+
+type CommandParams struct {
+	DefaultOriginalPath string
+	Config              *entity.Config
+}
+
+func Commands(cp *CommandParams) []cli.Command {
+	cmds := []cli.Command{
+		*init_(cp.DefaultOriginalPath, cp.Config),
+		*list(cp.Config),
+		*install(cp.Config),
+		*switch_(cp.Config),
+		*remove(cp.Config),
+		*rls(cp.Config),
+		*proxy(cp.Config),
+	}
+	return cmds
+}
