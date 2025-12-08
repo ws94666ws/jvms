b929d4d95190c3d98e26c8a56a2aa4b4a337448d
renniemaharaj <rvesprey@gmail.com>
2025-12-08 19:32:07 -0400

    feat: use command installs before doing switch

--- internal/cmdCli/install.go
+++ internal/cmdCli/install.go
@@ -18,69 +18,73 @@ func install(config *entity.Config) *cli.Command {
 		Name:      "install",
 		ShortName: "i",
 		Usage:     "Install available remote jdk",
-		Action: func(c *cli.Context) error {
-			if config.Proxy != "" {
-				web.SetProxy(config.Proxy)
-			}
-			v := c.Args().Get(0)
-			if v == "" {
-				return errors.New("invalid version., Type \"jvms rls\" to see what is available for install")
-			}
-
-			if jdk.IsVersionInstalled(config.Store, v) {
-				fmt.Println("Version " + v + " is already installed.")
-				return nil
-			}
-			versions, err := getJdkVersions(config)
-			if err != nil {
-				return err
-			}
-
-			if !file.Exists(config.Download) {
-				os.MkdirAll(config.Download, 0777)
-			}
-			if !file.Exists(config.Store) {
-				os.MkdirAll(config.Store, 0777)
-			}
-
-			for _, version := range versions {
-				if version.Version == v {
-					dlzipfile, success := web.GetJDK(config.Download, v, version.Url)
-					if success {
-						fmt.Printf("Installing JDK %s ...\n", v)
-
-						// Extract jdk to the temp directory
-						jdktempfile := filepath.Join(config.Download, fmt.Sprintf("%s_temp", v))
-						if file.Exists(jdktempfile) {
-							err := os.RemoveAll(jdktempfile)
-							if err != nil {
-								panic(err)
-							}
-						}
-						err := file.Unzip(dlzipfile, jdktempfile)
-						if err != nil {
-							return fmt.Errorf("unzip failed: %w", err)
-						}
-
-						// Copy the jdk files to the installation directory
-						temJavaHome := getJavaHome(jdktempfile)
-						err = os.Rename(temJavaHome, filepath.Join(config.Store, v))
-						if err != nil {
-							return fmt.Errorf("unzip failed: %w", err)
-						}
-
-						// Remove the temp directory
-						// may consider keep the temp files here
-						os.RemoveAll(jdktempfile)
-						fmt.Printf("Installation completedly succesfully. Use: jvms switch %v, if you'd like to use this version", v)
-					} else {
-						fmt.Println("Could not download JDK " + v + " executable.")
-					}
-					return nil
-				}
-			}
-			return errors.New("invalid version., Type \"jvms rls\" to see what is available for install")
-		},
+		Action:    installFunc(config),
 	}
 	return cmd
+}
+
+func installFunc(config *entity.Config) func(*cli.Context) error {
+	return func(c *cli.Context) error {
+		if config.Proxy != "" {
+			web.SetProxy(config.Proxy)
+		}
+		v := c.Args().Get(0)
+		if v == "" {
+			return errors.New("invalid version., Type \"jvms rls\" to see what is available for install")
+		}
+
+		if jdk.IsVersionInstalled(config.Store, v) {
+			fmt.Println("Version " + v + " is already installed.")
+			return nil
+		}
+		versions, err := getJdkVersions(config)
+		if err != nil {
+			return err
+		}
+
+		if !file.Exists(config.Download) {
+			os.MkdirAll(config.Download, 0777)
+		}
+		if !file.Exists(config.Store) {
+			os.MkdirAll(config.Store, 0777)
+		}
+
+		for _, version := range versions {
+			if version.Version == v {
+				dlzipfile, success := web.GetJDK(config.Download, v, version.Url)
+				if success {
+					fmt.Printf("Installing JDK %s ...\n", v)
+
+					// Extract jdk to the temp directory
+					jdktempfile := filepath.Join(config.Download, fmt.Sprintf("%s_temp", v))
+					if file.Exists(jdktempfile) {
+						err := os.RemoveAll(jdktempfile)
+						if err != nil {
+							panic(err)
+						}
+					}
+					err := file.Unzip(dlzipfile, jdktempfile)
+					if err != nil {
+						return fmt.Errorf("unzip failed: %w", err)
+					}
+
+					// Copy the jdk files to the installation directory
+					temJavaHome := getJavaHome(jdktempfile)
+					err = os.Rename(temJavaHome, filepath.Join(config.Store, v))
+					if err != nil {
+						return fmt.Errorf("unzip failed: %w", err)
+					}
+
+					// Remove the temp directory
+					// may consider keep the temp files here
+					os.RemoveAll(jdktempfile)
+					fmt.Printf("Installation completedly succesfully. Use: jvms switch %v, if you'd like to use this version", v)
+				} else {
+					fmt.Println("Could not download JDK " + v + " executable.")
+				}
+				return nil
+			}
+		}
+		return errors.New("invalid version., Type \"jvms rls\" to see what is available for install")
+	}
 }
